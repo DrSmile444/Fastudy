ec85d669133b94f4f675d4f58b71fa8243751c87
DrSmile444 <drsmile444@gmail.com>
2018-09-29 10:49:01 +0300

    added slider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  directionSlider: Array<Object> = [
+    {
+      name: "Кемпинг",
+      img: ""
+    },
+    {
+      name: "Медицина",
+      img: ""
+    },
+    {
+      name: "Программирование",
+      img: ""
+    }
+  ];
 
-  constructor(public navCtrl: NavController) {
+  directionList: Array<String> = [
+    "Программирование",
+    "Кэмпинг",
+    "Медицина",
+    "Рыбалка",
+    "Исскуство",
+    "Фильмы"
+  ];
 
-  }
-
+  constructor(public navCtrl: NavController) {}
 }
